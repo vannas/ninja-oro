11a531a984e253098542367d78f053d360387a57
Vannas <vdanessi@gmail.com>
2020-07-30 12:56:08 -0400

    Progreso hasta tarea anterior con store local

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from 'App.vue';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+    routes: [
+        {
+            path: '/',
+            component: App
+        }
+    ]
+})
